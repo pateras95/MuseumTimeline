f0ac74073db49b30e8f9233b0b7a733db8d3ab57
pateras95 <kostasmc2@gmail.com>
2021-02-09 23:01:06 +0200

    test commit

---
+++ wp-content/themes/bradbury/js/bradbury.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function($) { 
+	
+	/**
+	* FitVids - Responsive Videos in posts
+	*/
+	$("#site-main").fitVids();
+
+    $(function () {
+
+        $('.sf-menu').superfish({
+            'speed': 'fast',
+            'delay' : 0,
+            'animation': {
+                'height': 'show'
+            }
+        });
+
+    });
+
+    $(".site-toggle-anchor").click(function(){
+        $("#site-mobile-menu").toggleClass("is-visible");
+        $(".site-toggle-label").toggleClass("is-visible");
+        $(".site-toggle-icon").toggleClass("is-visible");
+
+    });
+
+    $(".sub-menu-toggle").click(function(){
+        $(this).next().toggleClass("is-visible");
+        $(this).toggleClass("is-visible");
+    });
+
+	jQuery(".site-flexslider").flexslider({
+	        selector: ".site-slideshow-list > .site-slideshow-item",
+		animation: "slide",
+		animationLoop: true,
+	        initDelay: 500,
+		smoothHeight: false,
+		slideshow: false,
+		slideshowSpeed: 5000,
+		pauseOnAction: true,
+		pauseOnHover: false,
+	        controlNav: false,
+		directionNav: true,
+		useCSS: true,
+		touch: false,
+	        animationSpeed: 600,
+		rtl: false,
+		reverse: false
+	});
+
+});
